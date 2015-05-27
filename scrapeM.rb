786bf5a3052514750d36ff93130d62196eb660d3
bhavita07 <bhavita.jaiswal07@gmail.com>
2015-05-27 15:34:24 -0500

    Giving the quote and an image

--- scrapeM.rb
+++ scrapeM.rb
@@ -17,12 +17,12 @@ page = mechanize.get(url)
 #Get the paragraph 
 
 
-
+count = 1 
 page.search('p').each do |p|
 	# puts p.text.strip
 	
 	
-	d=p.text.strip.split(".")
+	d=p.text.strip.split("./\s/")
 
 	# d.each do |line|
 	# 	if line.include? "  "
@@ -30,25 +30,86 @@ page.search('p').each do |p|
 	# 	end
 
 	# end
-
-	$count = "true"
+	
+	
 	d.each do |line|
-			# puts line
-			if $count == "true"
-				if line.start_with? ("\"") and line.include? ("I ")
-					puts line
-				end
-				# if line.start_with? ("I ") 
-				# 		$count = "false"
-				# 		puts line
+		 # puts line
+		if count == 1
+			if line.size > 50 and line.size <= 250
+				# puts line[0]
+
+				if line.start_with? ("/\"/+I+/\s/")
+					if line.include? (";")
+						count=0
+						 puts line
+						 puts "1"
+					
+					elsif line.include? ("_")
+						count=0
+						 puts line
+						 puts "2"
 						
-				# end
-				# if line.start_with? ("\"")
-				# 		$count = "false"
-				# 		 puts line
-				# end
+					
+					elsif line.include? (":")
+						count=0
+						 puts line
+						 puts "3"
+						
+					
+					elsif line.include? (",")
+						count=0
+						 puts line
+						 puts "4"
+						
+					else
+						count = 0
+						puts line
+						puts "5"
+					end
+
+				end
+				if line.start_with? ("I ")
+					if line.include? (";")
+						count=0
+						puts line
+						puts "6"
+					elsif line.include? ("_")
+					 	 
+						puts line
+						count=0
+						puts "7"
+					
+					elsif line.include? (":")
+						puts line
+						count=0
+						puts "8"
+					
+					elsif line.include? (",")
+						puts line
+						count=0
+						puts "9"
+					else
+						count = 0
+						puts line
+						puts "10"
+					end
+				end
+				if line.start_with? ("/\"/")
+						count = 0
+						 puts line
+						 puts "11"
+						 
+				end
+				if line.include? ("\"")
+						count = 0
+						 puts line
+						 puts "12"
+						 
+				end
+					
 			end
-			puts "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA \n"
+		end
+			 # puts "AAAAAAAAAAAAAAAAAAAAAA"
 	end
 	
 
@@ -65,19 +126,26 @@ page.search('p').each do |p|
 end
 
 
-string = "The force end will be with you always"
-my_match = /end/.match(string)
-puts my_match
+# string = "The force end will be with you always"
+# my_match = /end/.match(string)
+# puts my_match
 	
 	
 # if data["If "]
 # 	puts data.split(".")
 # end
 
-
+a=1
 #Get the URL of the images in the Article
-# page.search('img').each do |img|
-# 	puts img['src']
-# 	puts ''
-# end
+page.search('img').each do |img|
+	if a==1
+		a=0
+		puts img['src']
+		puts ''
+	end
+end
+
+# string= "Bhavita jaiswal"
+# puts "***************************************************"
+
 
